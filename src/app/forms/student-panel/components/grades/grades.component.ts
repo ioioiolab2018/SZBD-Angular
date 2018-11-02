5dccf0fc52331ce9b9deea3fdca837682d079294
Paweł Bubak <pawelbubak97@gmail.com>
2018-11-02 23:18:07 +0100

    Poprawka builda

--- src/app/forms/student-panel/components/grades/grades.component.ts
+++ src/app/forms/student-panel/components/grades/grades.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GradeInformation } from 'src/app/shared/model/grade-information';
 import { TableData } from 'src/app/shared/model/table-data';
+import { TableValue } from 'src/app/shared/model/table_value';
 
 @Component({
     selector: 'app-grades',
@@ -8,7 +9,7 @@ import { TableData } from 'src/app/shared/model/table-data';
     styleUrls: ['./grades.component.scss']
 })
 export class GradesComponent implements OnInit {
-    selectedSemester: number;
+    selectedSemester: string;
     grades: GradeInformation[];
     columns: string[];
     data: TableData[];
@@ -21,14 +22,14 @@ export class GradesComponent implements OnInit {
         ];
         this.columns = ['Semestr', 'Data zaliczenia', 'Ocena'];
         this.data = [
-            new TableData('I', '20.02.2018', '4.8'),
-            new TableData('II', '28.06.2018', '4.67')
+            new TableData(1, 'I', '20.02.2018', '4.8'),
+            new TableData(2, 'II', '28.06.2018', '4.67')
         ];
     }
 
     ngOnInit() {}
 
-    getGrades(val: number): void {
-        this.selectedSemester = val;
+    getGrades(val: TableValue): void {
+        this.selectedSemester = val.column;
     }
 }
