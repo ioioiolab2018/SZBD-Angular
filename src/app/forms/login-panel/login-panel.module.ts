8c42cabe20393c18e7fc57be2f759d03584cf551
Paweł Bubak <pawelbubak97@gmail.com>
2018-11-06 23:27:53 +0100

    Dodanie autoryzacji użytkownika

--- src/app/forms/login-panel/login-panel.module.ts
+++ src/app/forms/login-panel/login-panel.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginPanelComponent } from './login-panel.component';
 import { LoginPanelRoutingModule } from './login-panel-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule, LoginPanelRoutingModule],
+    imports: [CommonModule, LoginPanelRoutingModule, ReactiveFormsModule],
     declarations: [LoginPanelComponent]
 })
 export class LoginPanelModule {}
